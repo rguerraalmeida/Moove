f0c3e141d3db470809f61a4193c4209347fdc5ff
Rui Guerra (Almeida) <r.guerra.almeida@gmail.com>
2015-04-10 01:09:51 +0100

    Stuff

---
+++ Moove/MooveUI/Converters/ValueToForegroundConverter.cs
@@ -0,0 +1,61 @@
+﻿using MooveUI.Extensions;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace MooveUI.Converters
+{
+    //[ValueConversion(typeof(double), typeof(Brush))]
+    public class ValueToForegroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+        object parameter, CultureInfo culture)
+        {
+            double convertedValue = (double)value;
+
+            try
+            {
+                if (convertedValue.Between(-0.1, 0.1, true))
+                {
+                    return Application.Current.FindResource("IndexUnchanged");
+                }
+
+                //negative values
+                if (convertedValue.Between(-1, -0.1))
+                {
+                    return Application.Current.FindResource("IndexNegativeLowChange");
+                }
+
+                if (convertedValue <= -1)
+                {
+                    return Application.Current.FindResource("IndexNegativeHighChange");
+                }
+
+
+                if (convertedValue.Between(0.1, 1))
+                {
+                    return Application.Current.FindResource("IndexPositiveLowChange");
+                }
+
+                if (convertedValue >= 1)
+                {
+                    return Application.Current.FindResource("IndexPositiveHighChange");
+                }
+
+                return new SolidColorBrush(Colors.Yellow);
+            }
+            catch (Exception)
+            {
+                return new SolidColorBrush(Colors.Yellow);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Moove/MooveUI/MarketIndexesViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MooveUI
+{
+    public class MarketIndexesViewModel : NotificationObject
+    {
+        public ObservableCollection<SingleIndexViewModel> MarketIndexesColletion { get; set; }
+
+        public MarketIndexesViewModel()
+        {
+            MarketIndexesColletion = new ObservableCollection<SingleIndexViewModel>();
+
+            //EU
+            MarketIndexesColletion.Add(new SingleIndexViewModel("germany-30", "DAX"));
+            MarketIndexesColletion.Add(new SingleIndexViewModel("spain-35", "IBEX"));
+            MarketIndexesColletion.Add(new SingleIndexViewModel("psi-20", "PSI 20"));
+
+
+            //USA
+            MarketIndexesColletion.Add(new SingleIndexViewModel("us-30", "Dow Jones"));
+            MarketIndexesColletion.Add(new SingleIndexViewModel("nasdaq-composite", "Nasdaq"));
+            MarketIndexesColletion.Add(new SingleIndexViewModel("us-spx-500", "SP 500"));
+            MarketIndexesColletion.Add(new SingleIndexViewModel("nq-100", "Nasdaq 100"));
+
+            //EU
+            MarketIndexesColletion.Add(new SingleIndexViewModel("france-40", "CAC"));
+            MarketIndexesColletion.Add(new SingleIndexViewModel("uk-100", "FTSE UK"));
+            MarketIndexesColletion.Add(new SingleIndexViewModel("it-mib-40", "ITA MIB"));
+            MarketIndexesColletion.Add(new SingleIndexViewModel("eu-stoxx50", "EU-Stoxx50"));
+
+            //RUSSIA
+            MarketIndexesColletion.Add(new SingleIndexViewModel("mcx", "RUS MICEX"));
+            MarketIndexesColletion.Add(new SingleIndexViewModel("rtsi", "RUS RTSI"));
+        }
+    }
+}
